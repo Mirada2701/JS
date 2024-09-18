d8d03bab1801fc181d68bf595bfd797cf3d7f245
Віталій Лабенський <vitaliy.laben@gmail.com>
2024-09-18 23:26:06 +0300

    Forms HW

---
+++ 04_Forms/main.js
@@ -0,0 +1,21 @@
+const mainDiv = document.getElementById('main');
+console.log(mainDiv);
+
+const form = document.forms.products;
+const name = form.elements.title;
+const select = form.elements.type;
+
+form.onsubmit = (event) => {
+    event.preventDefault();
+    const path = `./images/${select.value}`;
+    const title = select.value.split('.')[0];
+    
+    mainDiv.innerHTML += ` <div class="item">
+            <img src="${path}">
+            <p>${title}</p>
+        </div>;`
+    
+}
+
+
+
