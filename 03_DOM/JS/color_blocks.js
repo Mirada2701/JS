60df26bcd5997c09772dcffd47997acf0add5360
Віталій Лабенський <vitaliy.laben@gmail.com>
2024-09-20 15:23:10 +0300

    Local Storage HW

--- 03_DOM/JS/color_blocks.js
+++ 03_DOM/JS/color_blocks.js
@@ -1,19 +1,42 @@
+window.onload = () => {
+    load();
+}
+
  const addBlockBtn = document.getElementById('addBlockBtn');
  const container = document.getElementById('container');
 
- addBlockBtn.onclick = () => {
-    const block = document.createElement('div');
-     block.className = 'color-block';
+function addBlockToCont(block) {
+    container.innerHTML += `<div class="color-block" style="background-color: rgb(${block.r}, ${block.g}, ${block.b});"></div>`
+}
 
-     const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-     block.style.backgroundColor = randomColor;
+let blocks = [];
 
-     
-     block.addEventListener('dblclick', function() {
-         container.removeChild(block);
-     });
+ addBlockBtn.onclick = (e) => {
+     e.preventDefault();
+     let block = {
+         r: Math.floor(Math.random() * 256),
+         g: Math.floor(Math.random() * 255),
+         b: Math.floor(Math.random() * 255)
+     }
+     blocks.push(block);
+     save();
 
-     container.appendChild(block);
+
+     addBlockToCont(block);
  }
+
+ function save() {
+
+    localStorage.setItem("block-list", JSON.stringify(blocks));
+}
+
+function load() {
+    blocks = JSON.parse(localStorage.getItem("block-list"));
+console.log(blocks);
+
+    for (const b of blocks) {
+        addBlockToCont(b);
+    }
+}
 
 
